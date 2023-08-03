66101f8ef24a8558caeb7481ac02e21800b27136
piygr <grover1piyush@gmail.com>
2023-08-03 23:54:09 +0530

    code update

--- models/custom_resnet_lightning_s10.py
+++ models/custom_resnet_lightning_s10.py
@@ -195,15 +195,16 @@ class S10LightningModel(pl.LightningModule):
 
 
     def on_validation_epoch_end(self):
-        print('Epoch ', self.current_epoch)
-        print('Train Accuracy', 100 * self.acc['train'] / self.acc['train_total'], '% [',
-              self.acc['train'], '/', self.acc['train_total'], ']')
-        self.acc['train'] = 0
-        self.acc['train_total'] = 0
+        if self.acc['train_total']:
+            print('Epoch ', self.current_epoch)
+            print('Train Accuracy', 100 * self.acc['train'] / self.acc['train_total'], '% [',
+                  self.acc['train'], '/', self.acc['train_total'], ']')
+            self.acc['train'] = 0
+            self.acc['train_total'] = 0
 
-        print('Validation Accuracy', 100 * self.acc['val'] / self.acc['val_total'], '% [', self.acc['val'], '/', self.acc['val_total'], ']')
-        self.acc['val'] = 0
-        self.acc['val_total'] = 0
+            print('Validation Accuracy', 100 * self.acc['val'] / self.acc['val_total'], '% [', self.acc['val'], '/', self.acc['val_total'], ']')
+            self.acc['val'] = 0
+            self.acc['val_total'] = 0
 
 
 
