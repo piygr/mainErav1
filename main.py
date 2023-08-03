bc6eb432f7183255ae87d57f8752d0569e375bfb
piygr <grover1piyush@gmail.com>
2023-08-03 23:31:22 +0530

    code update

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def init(network=None, show_sample=True, show_model_summary=True, find_lr=False,
 
         if start_train:
             trainer = pl.Trainer(
-                max_epochs=4
+                max_epochs=6
             )
             trainer.fit(model, train_dataloader, test_dataloader)
 
