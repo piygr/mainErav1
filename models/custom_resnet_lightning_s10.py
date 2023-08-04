8d431726fc171189550bf2e7e7fe23653729757d
piygr <grover1piyush@gmail.com>
2023-08-05 00:11:10 +0530

    code update

--- models/custom_resnet_lightning_s10.py
+++ models/custom_resnet_lightning_s10.py
@@ -194,7 +194,7 @@ class S10LightningModel(pl.LightningModule):
         scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,
                                                   max_lr=self.max_lr,
                                                   epochs=self.trainer.max_epochs,
-                                                  total_steps=self.trainer.estimated_stepping_batches,
+                                                  steps_per_epoch=len(self.train_dataloader()),
                                                   pct_start=5 / self.trainer.max_epochs,
                                                   div_factor=100,
                                                   final_div_factor=100,
